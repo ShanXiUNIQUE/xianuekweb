835ac050190fef94ce21693544016091df2e2c77
Allcky <allcky@qq.com>
2016-10-08 20:54:02 +0800

    add swiper

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$(function(){
+    var mySwiper = new Swiper('.hnz-swiper-container',{
+        // pagination: '.pagination',
+        // loop:true,
+        // grabCursor: true,
+        // paginationClickable: true
+    })
+    $('.hnz-arrow-left').on('click', function(e){
+        e.preventDefault()
+        mySwiper.swipePrev()
+    })
+    $('.hnz-arrow-right').on('click', function(e){
+        e.preventDefault()
+        mySwiper.swipeNext()
+    })
+})
