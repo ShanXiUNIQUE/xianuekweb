ebab37932b7aa8303e44731accee6c145dd616eb
李星 <1459294281@qq.com>
2016-10-10 09:05:55 +0800

    footer

--- js/index2.js
+++ js/index2.js
@@ -1,6 +1,6 @@
 $(function(){
     // 就业成果选项卡
-    var lx_tabsSwiper = new Swiper('.swiper-container',{
+    var lx_tabsSwiper = new Swiper('.lx_tabBox',{
         onlyExternal : true,
         speed:500
     })
@@ -20,4 +20,58 @@ $(function(){
     },function(){
         $(this).find('.lx_tabMask').animate({left:'-100%'},'fast','swing')
     })
+
+    // it梦启航
+    var lx_tabsSwiper1 = new Swiper('.lx_dreamTab',{
+        onlyExternal : true,
+        speed:500
+    })
+    $(".lx_dreamItem li").on('touchstart mousedown',function(e){
+        e.preventDefault()
+        $(".lx_dreamItem li").removeClass('lx_dreamHot')
+        $(this).addClass('lx_dreamHot')
+        lx_tabsSwiper1.swipeTo( $(this).index() )
+    })
+    $(".lx_dreamItem li").click(function(e){
+        e.preventDefault()
+    })
+
+
+    // 媒体报道 获得奖项
+    var lx_tabsSwiper2 = new Swiper('.lx_subject .lx_tabBox',{
+        onlyExternal : true,
+        speed:500
+    })
+    $(".lx_subject .lx_gainMenu li").on('touchstart mousedown',function(e){
+        e.preventDefault()
+        $(".lx_subject .lx_gainMenu .lx_menuHot").removeClass('lx_menuHot')
+        $(this).addClass('lx_menuHot')
+        lx_tabsSwiper2.swipeTo( $(this).index() )
+    })
+    $(".lx_subject .lx_gainMenu li").click(function(e){
+        e.preventDefault()
+    })
+//    权威认证  教学环境
+    var lx_tabsSwiper3 = new Swiper('.lx_accept .lx_tabBox',{
+        onlyExternal : true,
+        speed:500
+    })
+    $(".lx_accept .lx_gainMenu li").on('touchstart mousedown',function(e){
+        e.preventDefault()
+        $(".lx_accept .lx_gainMenu .lx_menuHot").removeClass('lx_menuHot')
+        $(this).addClass('lx_menuHot')
+        lx_tabsSwiper3.swipeTo( $(this).index() )
+    })
+    $(".lx_accept .lx_gainMenu li").click(function(e){
+        e.preventDefault()
+    })
+
+//    辐射城市
+    var  city=$(".lx_radiaCity");
+    var  cityArr=[[193,174],[392,105],[456,222],[452,323],[407,377],[223,356],[160,262]];
+    city.each(function(index,obj){
+         $(obj).css({left:cityArr[index][0],top:cityArr[index][1]});
+        console.log(cityArr[obj])
+    })
+
 })
