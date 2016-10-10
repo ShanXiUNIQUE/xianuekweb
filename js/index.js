019b24fff996214872a218b7b790d3cab6b794a6
Allcky <allcky@qq.com>
2016-10-10 11:30:03 +0800

    就业服务链

--- js/index.js
+++ js/index.js
@@ -23,7 +23,7 @@ $(function(){
         $courseList.eq(1).addClass('hot');
     })
 
-    // $('.team-swiper-container .swiper-slide:nth-child(4n)').css({marginRight:0})
+    //师资团队
     var teamWheel = new Swiper('.team-swiper-container',{
         loop:true,
         autoplay : 2000,
@@ -31,7 +31,39 @@ $(function(){
         pagination: '.hnz-pagination',
         paginationClickable: true,
         autoplayDisableOnInteraction:true,
-        // slidesPerGroup:4,
         cssWidthAndHeight:false
     })
+
+    var tabsSwiper = new Swiper('.swiper-container',{
+    onlyExternal : true,
+    speed:500
+  })
+  $(".tabs div").on('touchstart mousedown',function(e){
+    e.preventDefault()
+    $(".tabs .active").removeClass('active')
+    $(this).addClass('active')
+    tabsSwiper.swipeTo( $(this).index() )
+  })
+
+  new Swiper('.production-list .swiper-container',{
+        ploop:true,
+        autoplay : 2000,
+        slidesPerView:4,
+        // pagination: '.pagination',
+        // paginationClickable: true,
+        autoplayDisableOnInteraction:true,
+        cssWidthAndHeight:false
+  })
+
+  var divs = $('.job-list div');
+  var jobMask = $('.job-mask');
+  var iw = divs.width();
+  divs.hover(function(){
+      var i = $(this).index();
+      divs.css({color:"#555"})
+      var that = this;
+      jobMask.animate({left:i*iw},200,function(){
+           $(that).css({color:"#fff"})
+      })
+  })
 })
